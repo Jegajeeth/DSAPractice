0e3a9a9f1557222047a932f275fdfebae471d1c5
Jegajeeth Manikandan <Jegajeeth.Manikandan@nafinc.com>
2026-08-09 15:47:12 +0530

    add(mit6006): added a solution for the problem set 0.

--- Problems6006Solution/problemOne.py
+++ Problems6006Solution/problemOne.py
@@ -1,1 +1,26 @@
-# problem 1.3
+# problem solution for the problem set 0;
+
+a = (1,3,4,2,7,5,6,9,8);
+i = 0;
+longestSumArrays = {};
+maxcount = 0;
+    
+while(i < len(a)):
+    currentTupEle = a[i];
+    longestSumArray = [currentTupEle];
+    while((i != len(a) - 1) and (a[i + 1] - currentTupEle >= 0)):
+        longestSumArray.append(a[i+1]);
+        currentTupEle = a[i + 1];
+        i += 1;
+    count = len(longestSumArray);
+    if(count not in longestSumArrays.keys()):
+        longestSumArrays[count] = [];
+    longestSumArrays[count].append(longestSumArray);
+    if(count > maxcount): 
+        maxcount = count;
+    longestSumArray = [];
+    i += 1;
+
+print(f"longestSumArrays = {longestSumArrays}");
+print(f"maxcount = {maxcount}");
+print(f"count of largest sumsubarray = {len(longestSumArrays[maxcount])}")
